efbe1e9f4066978c95d1357435ed56e8657ad532
Shark <cuong28022003@gmail.com>
2024-12-09 06:16:02 +0700

    fix filter and add empty notification

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -3,12 +3,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { loginSuccess } from "../redux/authSlice";
 import { toast } from "react-toastify";
 import apiMain from "../api/apiMain";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 
 const Saved = (props) => {
   const data = props.data;
   const user = useSelector((state) => state.auth.login.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   
 
   const handleUnsaveComic = async () => {
@@ -29,13 +31,20 @@ const Saved = (props) => {
     }
   };
 
+  const onClickTruyen = (e) => {
+    //xử lý click vào tên truyện để đọc
+    navigate("/truyen/" + e.target.name); //điều hướng web
+  };
+
   return (
     <div className="saved-item">
       <div className="saved-item__img-wrap">
         <img src={data.image} alt={data.name} />
       </div>
       <div className="saved-item__content">
-        <h3 className="saved-item__title">{data.name}</h3>
+        <a onClick={onClickTruyen} name={data?.url} className="reading-card__title">
+          {data.name}
+        </a>
         <p className="saved-item__info">Tác giả: {data.artist}</p>
         <button className="btn-remove" onClick={() => handleUnsaveComic()}>
           Bỏ đánh dấu
