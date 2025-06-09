5d16719c3f09a360454bf8e910decc91543ba0b3
Shark <cuong28022003@gmail.com>
2025-06-09 15:49:03 +0700

    add logout button admin page

--- src/views/Admin/index.jsx
+++ src/views/Admin/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import "./styles.scss";
+import { useDispatch } from "react-redux";
+import { handleLogout } from "../../handle/handleAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AdminPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const onClickLogout = () => {
+    handleLogout(dispatch, navigate, location)
+  }
   return (
     <div className="admin-container">
       <div className="sidebar-menu">
@@ -14,6 +23,12 @@ const AdminPage = () => {
           <NavLink to="ranks" className={({ isActive }) => isActive ? "active" : ""}>Quản lý rank</NavLink>
           <NavLink to="topups" className={({ isActive }) => isActive ? "active" : ""}>Quản lý nạp kim cương</NavLink>
         </nav>
+        <button
+          className="logout-btn"
+          onClick={onClickLogout}
+        >
+          Đăng xuất
+        </button>
       </div>
       <div className="admin-content">
         <Outlet />
