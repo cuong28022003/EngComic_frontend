e9ecf267022e9aafc78b462e61e0acdebc0b6916
Shark <cuong28022003@gmail.com>
2025-11-22 09:37:13 +0700

    update genres

--- src/constant/enum.js
+++ src/constant/enum.js
@@ -1,32 +1,32 @@
-// export const ComicGenres = [
-//     'Hành động',
-//     'Phiêu lưu',
-//     'Hài hước',
-//     'Chính kịch',
-//     'Giả tưởng',
-//     'Kinh dị',
-//     'Bí ẩn',
-//     'Lãng mạn',
-//     'Khoa học viễn tưởng',
-//     'Đời thường',
-//     'Thể thao',
-//     'Siêu nhiên',
-//     'Giật gân',
-//     'Khác',
-// ];
-
-// Adult only
 export const ComicGenres = [
-    'Ecchi',
-    'Hentai',
-    'Yaoi',
-    'Yuri',
-    'Smut',
-    'Doujinshi',
-    'Adult',
-    'Mature',
-    'Erotica',
+    'Hành động',
+    'Phiêu lưu',
+    'Hài hước',
+    'Chính kịch',
+    'Giả tưởng',
+    'Kinh dị',
+    'Bí ẩn',
+    'Lãng mạn',
+    'Khoa học viễn tưởng',
+    'Đời thường',
+    'Thể thao',
+    'Siêu nhiên',
+    'Giật gân',
+    'Khác',
 ];
+
+// // Adult only
+// export const ComicGenres = [
+//     'Ecchi',
+//     'Hentai',
+//     'Yaoi',
+//     'Yuri',
+//     'Smut',
+//     'Doujinshi',
+//     'Adult',
+//     'Mature',
+//     'Erotica',
+// ];
 
 export const AgeRatings = [
     'Kids',
